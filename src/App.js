5b1957315071637bdf74491ff6805412c71fdf3f
Shoaib Shabir <AIwithshoaib@gmail.com>
2024-01-02 23:58:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          sue wati ghari
         </a>
       </header>
     </div>
